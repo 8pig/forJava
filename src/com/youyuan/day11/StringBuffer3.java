9726014968e422590a50320bbc5d8dfbfa10975e
zhou <zhou773@foxmail.com>
2019-04-17 14:38:37 +0800

    add StringBuffer

--- src/com/youyuan/day11/StringBuffer3.java
+++ src/com/youyuan/day11/StringBuffer3.java
@@ -3,7 +3,7 @@ package com.youyuan.day11;
 public class StringBuffer3 {
     public static void main(String[] args) {
         StringBuffer sb = new StringBuffer("yezhupeiqi");
-        sb.replace(0,1,"hei");
+        sb.replace(0,1,"sb");
         System.out.println(sb);
     }
 }
