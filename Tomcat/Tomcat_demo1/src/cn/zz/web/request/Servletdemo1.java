c5ab80577e9c05e0853e28f87009f4cb64eaead2
zhou <zhou773@foxmail.com>
2019-06-14 18:29:01 +0800

    m

--- Tomcat/Tomcat_demo1/src/cn/zz/web/request/Servletdemo1.java
+++ Tomcat/Tomcat_demo1/src/cn/zz/web/request/Servletdemo1.java
@@ -1,10 +1,26 @@
-@javax.servlet.annotation.WebServlet(name = "Servletdemo1")
-public class Servletdemo1 extends javax.servlet.http.HttpServlet {
-    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+package cn.zz.web.request;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Enumeration;
+
+@WebServlet("/Servletdemo1")
+public class Servletdemo1 extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
-    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while( headerNames.hasMoreElements() ){
+            String name = headerNames.nextElement();
+            String value = request.getHeader(name);
+            System.out.println(name +','+ value);
 
+        }
     }
 }
