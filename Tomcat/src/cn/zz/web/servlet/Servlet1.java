be97d0a832a3fc0ead7569e6e4444bb35d337728
zhou <zhou773@foxmail.com>
2019-06-15 22:41:58 +0800

    dsds dsa dsa ,

--- Tomcat/src/cn/zz/web/servlet/Servlet1.java
+++ Tomcat/src/cn/zz/web/servlet/Servlet1.java
@@ -1,10 +1,34 @@
-@javax.servlet.annotation.WebServlet(name = "Servlet1")
-public class Servlet1 extends javax.servlet.http.HttpServlet {
-    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+package cn.zz.web.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+@WebServlet("/Servlet1")
+public class Servlet1 extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        // 获取消息体
+//        BufferedReader reader = request.getReader();
+//        // 读取数据
+//        String data = null;
+//        while ((data = reader.readLine())!= null ){
+//            System.out.println(data);
+//        }
+//        System.out.println("----------");
+
+        String username = request.getParameter("username");
+        System.out.println("post"+":"+username);
+
 
     }
 
-    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
-
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        System.out.println("get"+":"+username);
+        request.getRequestDispatcher("/servlet2").forward(request,response);
     }
 }
