7d513e3bc9638d48cf26ce312c257fea7d1e15f0
zhaozhou <zhou773@foxmail.com>
2021-10-21 11:02:29 +0800

    表字段影射

--- springboot04/src/main/java/com/zhou/springboot04/pojo/User.java
+++ springboot04/src/main/java/com/zhou/springboot04/pojo/User.java
@@ -1,12 +1,16 @@
 package com.zhou.springboot04.pojo;
 
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 @Data
+//@TableName("user")
 public class User {
 
     private Long id;
 
+    @TableField("name") // user_name 表字段直接转换为userName 驼峰
     private String name;
 
     private String email;
